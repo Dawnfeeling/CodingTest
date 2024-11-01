cabb44534607b02eb9b25688312c44e08ca7a52d
Dawnfeeling <swhan98@naver.com>
2024-11-01 09:44:34 +0900

    24/11/01

---
+++ src/programmers/level0/두정수사이의합/Solution.java
@@ -0,0 +1,17 @@
+package programmers.level0.두정수사이의합;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (b >= a) {
+            for (long i = a; i <= b; i++) {
+                answer = answer + i;
+            }
+        } else {
+            for (long i = b; i <= a; i++) {
+                answer = answer + i;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/programmers/level0/콜라츠추측/Solution.java
@@ -0,0 +1,22 @@
+package programmers.level0.콜라츠추측;
+
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long temp = num;
+        while (temp != 1) {
+            if (temp % 2 == 0) {
+                temp = temp / 2;
+                answer++;
+            } else {
+                temp = (temp * 3) + 1;
+                answer++;
+            }
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
